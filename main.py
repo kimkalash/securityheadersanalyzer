d6a6c6258e3884152b1151f83575b7df6722fac4
kimkalash <Sanurussia@gmail.com>
2025-08-27 16:43:27 +0000

    refactoring

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI
 from app.db import Base, engine
-from app.routes import users, scans, auth_routes
+from app.routes import users, auth, scans
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(auth.router)
 app.include_router(scans.router)
-app.include_router(auth_routes.router)
-
-@app.get("/")
-def root():
-    return {"message": "Security Header Analyzer is running"}
 
 
